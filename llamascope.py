5b0e302f234fd62c8b5b1f971ba09dbf2b602717
tomMcGrath <thomas.m.mcgrath@gmail.com>
2024-04-24 20:49:10 +0100

    change llamascope to first special-case to activations caching

--- llamascope.py
+++ llamascope.py
@@ -2,19 +2,13 @@ from dataclasses import dataclass
 import torch
 
 
-@dataclass
-class HookKeeper:
-   name: str
-   hook_fn: callable
-   hook_handle: None | torch.utils.hooks.RemovableHandle  # None before attached
-
-
 class LlamaScope:
     """Class for adding, using, and removing PyTorch hooks with a model."""
 
     def __init__(self, model):
         self.model = model
-        self.hooks = []
+        self.hooks = {}
+        self.activations_cache = {}
         self._build_module_dict()
 
 
@@ -34,8 +28,40 @@ class LlamaScope:
         """Lists all modules in the module dictionary."""
         return self._module_dict.keys()
     
-    def add_fwd_hook(self, hook: HookKeeper, module_str: str):
-        """Registers hook on the module given by module_str."""
-        handle = self._module_dict[module_str].register_forward_hook(hook.hook_fn)
-        hook.handle = handle
-        self.hooks.append(hook)
+    """Activations caching"""
+    def _build_caching_hook(self, module_str):
+        self.activations_cache[module_str] = []
+        def hook_fn(model, input, output):
+            self.activations_cache[module_str].append(output)
+
+        return hook_fn
+
+    def add_caching_hook(self, module_str):
+        """Adds an activations caching hook at the location in module_str."""
+        hook_fn = self._build_caching_hook(module_str)
+        hook_handle = self.model.register_forward_hook(hook_fn)
+        self.hooks['cache-'+module_str] = hook_handle
+
+    def clear_cache(self, module_str):
+        """Clears the activations cache corresponding to module_str."""
+        if module_str not in self.activations_cache.keys():
+            raise KeyError(f'No activations cache for {module_str}.')
+        
+        else:
+            self.activations_cache[module_str] = []
+
+    def clear_all_caches(self):
+        """Clear all activation caches."""
+        for module_str in self.activations_cache.keys():
+            self.clear_cache(module_str)
+
+    """Hook clearup"""
+    def remove_hook(self, hook_name):
+        """Remove a hook with name hook_name from the model."""
+        self.hooks[hook_name].remove()
+        del self.hooks[hook_name]
+
+    def remove_all_hooks(self):
+        """Remove all hooks from the model."""
+        for hook_name in self.hooks.keys():
+            self.remove_hook(hook_name)
